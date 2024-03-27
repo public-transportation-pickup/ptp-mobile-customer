3743474378f5dee5f25000a1531ed043e5c9d67a
Trọng Nghĩa <ptnghia3502@gmail.com>
2024-03-28 01:12:58 +0700

    add list transaction to wallet page

--- lib/pages/main_pages/wallet_page.dart
+++ lib/pages/main_pages/wallet_page.dart
@@ -1,8 +1,13 @@
+import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 import 'package:capstone_ptp/models/wallet_model.dart';
+import 'package:flutter/widgets.dart';
+import 'package:flutter_svg/flutter_svg.dart';
 import 'package:intl/intl.dart';
 
+import '../../models/transaction_model.dart';
 import '../../services/api_services/wallet_api.dart';
+import '../activity_pages/transaction_history_tab.dart';
 
 class WalletPage extends StatefulWidget {
   final String userId;
@@ -40,114 +45,175 @@ class _WalletPageState extends State<WalletPage> {
         ),
         centerTitle: true,
       ),
-      body: Center(
-        child: FutureBuilder<Wallet>(
-          future: _walletFuture,
-          builder: (context, snapshot) {
-            if (snapshot.connectionState == ConnectionState.waiting) {
-              return const CircularProgressIndicator();
-            } else if (snapshot.hasError) {
-              return Text('Có lỗi xảy ra vui lòng thử lại: ${snapshot.error}');
-            } else {
-              final wallet = snapshot.data!;
-              return Column(
-                mainAxisAlignment: MainAxisAlignment.center,
+      body: FutureBuilder<Wallet>(
+        future: _walletFuture,
+        builder: (context, snapshot) {
+          if (snapshot.connectionState == ConnectionState.waiting) {
+            return const Center(child: CircularProgressIndicator());
+          } else if (snapshot.hasError) {
+            return Text('Có lỗi xảy ra vui lòng thử lại: ${snapshot.error}');
+          } else {
+            final wallet = snapshot.data!;
+            List<Transaction>? transactions = wallet.transactions;
+            return SingleChildScrollView(
+              child: Column(
+                mainAxisAlignment: MainAxisAlignment.start,
                 children: [
-                  Stack(
-                    alignment: Alignment.center,
-                    children: [
-                      Container(
-                        width: 220,
-                        height: 220,
-                        decoration: BoxDecoration(
-                          shape: BoxShape.circle,
-                          border: Border.all(
-                            color: const Color(0xFFFCCF59),
-                            width: 10.0,
+                  SizedBox(
+                    width: MediaQuery.of(context).size.width * 0.95,
+                    height: 220,
+                    child: Stack(children: [
+                      Padding(
+                        padding: const EdgeInsets.all(16.0),
+                        child: Stack(children: [
+                          SizedBox(
+                            width: MediaQuery.of(context).size.width * 0.85,
+                            height: 220,
+                            child: SvgPicture.asset(
+                              'lib/assets/images/container_wallet.svg',
+                              alignment: Alignment.topCenter,
+                              fit: BoxFit.contain,
+                            ),
                           ),
-                        ),
-                        child: Column(
-                          mainAxisAlignment: MainAxisAlignment.center,
-                          children: [
-                            const Text(
-                              'Số dư hiện tại (VND):',
-                              style: TextStyle(fontSize: 16),
+                          Positioned(
+                            top: 10,
+                            right: 30,
+                            child: Row(
+                              mainAxisAlignment: MainAxisAlignment.end,
+                              crossAxisAlignment: CrossAxisAlignment.start,
+                              children: [
+                                Row(
+                                  crossAxisAlignment: CrossAxisAlignment.center,
+                                  children: [
+                                    IconButton(
+                                      onPressed: () {
+                                        setState(() {
+                                          _hideAmount = !_hideAmount;
+                                        });
+                                      },
+                                      icon: Icon(
+                                        _hideAmount
+                                            ? Icons.visibility
+                                            : Icons.visibility_off,
+                                        color: Colors.grey,
+                                        size: 20,
+                                      ),
+                                    ),
+                                    const Text(
+                                      "Số dư hiện tại",
+                                      style: TextStyle(
+                                        fontSize: 16,
+                                        fontWeight: FontWeight.w700,
+                                        color: Colors.white,
+                                      ),
+                                      textAlign: TextAlign.right,
+                                    ),
+                                  ],
+                                ),
+                              ],
                             ),
-                            const SizedBox(height: 5),
-                            Text(
-                              _hideAmount
-                                  ? '***'
-                                  : formatPrice(wallet.amount.toDouble()),
-                              style: const TextStyle(
-                                  fontSize: 20, fontWeight: FontWeight.bold),
+                          ),
+                          Positioned(
+                            top: 50,
+                            right: 30,
+                            child: SizedBox(
+                              width: 160,
+                              child: Row(
+                                mainAxisAlignment: MainAxisAlignment.end,
+                                crossAxisAlignment: CrossAxisAlignment.start,
+                                children: [
+                                  Flexible(
+                                    child: Text(
+                                      _hideAmount
+                                          ? '*****'
+                                          : formatPrice(
+                                              wallet.amount.toDouble()),
+                                      style: const TextStyle(
+                                        fontSize: 20,
+                                        fontWeight: FontWeight.bold,
+                                        color: Colors.white,
+                                      ),
+                                      softWrap: true,
+                                    ),
+                                  ),
+                                  const SizedBox(width: 8),
+                                  SvgPicture.asset(
+                                    'lib/assets/icons/tabler_coin_icon.svg',
+                                    alignment: Alignment.center,
+                                    fit: BoxFit.contain,
+                                  ),
+                                ],
+                              ),
                             ),
-                          ],
-                        ),
+                          ),
+                        ]),
                       ),
                       Positioned(
-                        bottom: 10,
-                        child: IconButton(
-                          onPressed: () {
-                            setState(() {
-                              _hideAmount = !_hideAmount;
-                            });
-                          },
-                          icon: Icon(
-                            _hideAmount
-                                ? Icons.visibility
-                                : Icons.visibility_off,
-                            color: Colors.grey,
-                            size: 30,
+                        top: 60,
+                        left: 0,
+                        child: SizedBox(
+                          width: 160,
+                          height: 160,
+                          child: SvgPicture.asset(
+                            'lib/assets/images/money.svg',
+                            alignment: Alignment.center,
+                            fit: BoxFit.contain,
                           ),
                         ),
                       ),
-                    ],
-                  ),
-                  const SizedBox(height: 20),
-                  Row(
-                    mainAxisAlignment: MainAxisAlignment.spaceEvenly,
-                    children: [
-                      Column(
-                        children: [
-                          InkWell(
-                            onTap: () {},
-                            child: Container(
-                              height: 48,
-                              width: 48,
-                              decoration: BoxDecoration(
-                                color: const Color(0xFFFEEBBB),
-                                borderRadius: BorderRadius.circular(15),
-                              ),
-                              child: const Icon(Icons.credit_card),
-                            ),
-                          ),
-                          const Text('Nạp tiền'),
-                        ],
+                      const Positioned(
+                        top: 160,
+                        right: 40,
+                        child: Text(
+                          "Nạp tiền >",
+                          style: TextStyle(
+                              fontSize: 16,
+                              fontWeight: FontWeight.w700,
+                              color: Colors.white),
+                          textAlign: TextAlign.right,
+                        ),
                       ),
-                      Column(
-                        children: [
-                          InkWell(
-                            onTap: () {},
-                            child: Container(
-                              height: 48,
-                              width: 48,
-                              decoration: BoxDecoration(
-                                color: const Color(0xFFFEEBBB),
-                                borderRadius: BorderRadius.circular(15),
-                              ),
-                              child: const Icon(Icons.add_link_outlined),
-                            ),
-                          ),
-                          const Text('Liên kết ngân hàng'),
-                        ],
-                      ),
-                    ],
+                    ]),
                   ),
+
+                  // list for history transaction
+                  const Padding(
+                    padding: EdgeInsets.fromLTRB(16, 16, 16, 8),
+                    child: Row(
+                      mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                      children: [
+                        Text(
+                          "Lịch sử giao dịch",
+                          style: TextStyle(
+                            fontSize: 20,
+                            fontWeight: FontWeight.bold,
+                            color: Colors.black,
+                          ),
+                          textAlign: TextAlign.left,
+                        ),
+                        Icon(
+                          Icons.arrow_forward_ios_rounded,
+                          color: Colors.black,
+                          size: 20,
+                        ),
+                      ],
+                    ),
+                  ),
+                  if (transactions.isNotEmpty)
+                    ListView.builder(
+                      shrinkWrap: true,
+                      physics: const NeverScrollableScrollPhysics(),
+                      itemCount: transactions.length,
+                      itemBuilder: (context, index) {
+                        return TransactionCard(
+                            transaction: transactions[index]);
+                      },
+                    ),
                 ],
-              );
-            }
-          },
-        ),
+              ),
+            );
+          }
+        },
       ),
     );
   }
